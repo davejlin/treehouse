1c5666406f60c97330c20199733217d1edf2502a
David Lin <David.Lin@bmwna.com>
2016-12-29 14:39:16 -0600

    Django learning site: add view for course list

--- python/django/learning_site/courses/views.py
+++ python/django/learning_site/courses/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+from .models import Course
+
 # Create your views here.
+def course_list(request):
+    course_list = ""
+    for course in Course.objects.all():
+        course_list += "<p>"+str(course)+"</p>"
+    return HttpResponse(course_list)
