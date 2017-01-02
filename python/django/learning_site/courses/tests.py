4ad9d1a0aa1158cab359a4cf7a7cf4299a623575
David Lin <David.Lin@bmwna.com>
2017-01-02 14:05:57 -0600

    Django learning site: complete Step model creation unit test

--- python/django/learning_site/courses/tests.py
+++ python/django/learning_site/courses/tests.py
@@ -2,7 +2,7 @@ from django.test import TestCase
 from django.utils import timezone
 
 # Create your tests here.
-from .models import Course
+from .models import Course, Step
 
 class CourseModelTests(TestCase):
     def test_course_creation(self):
@@ -14,10 +14,16 @@ class CourseModelTests(TestCase):
         self.assertLess(course.created_at, now)
 
 class StepModelTests(TestCase):
-    def test_course_creation(self):
+    def test_step_creation(self):
         course = Course.objects.create(
+            title="Python Regular Expressions",
+            description="Learn to write regular expressions in Python"
+        )
+
+        step = Step.objects.create(
             title="Step 1",
-            description="This is step 1"
+            description="This is step 1",
+            course = course
         )
         now = timezone.now()
-        self.assertLess(course.created_at, now)
+        self.assertLess(step.course.created_at, now)
