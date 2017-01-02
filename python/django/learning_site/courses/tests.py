53ebec6e5c7351bac37cefc566e87d6c82565dff
David Lin <David.Lin@bmwna.com>
2017-01-02 14:14:23 -0600

    Django learning site: add unit test for course view

--- python/django/learning_site/courses/tests.py
+++ python/django/learning_site/courses/tests.py
@@ -1,3 +1,4 @@
+from django.core.urlresolvers import reverse
 from django.test import TestCase
 from django.utils import timezone
 
@@ -14,16 +15,39 @@ class CourseModelTests(TestCase):
         self.assertLess(course.created_at, now)
 
 class StepModelTests(TestCase):
-    def test_step_creation(self):
-        course = Course.objects.create(
+    def setUp(self):
+        self.course = Course.objects.create(
             title="Python Regular Expressions",
             description="Learn to write regular expressions in Python"
         )
 
+    def test_step_creation(self):
         step = Step.objects.create(
             title="Step 1",
             description="This is step 1",
-            course = course
+            course = self.course
         )
         now = timezone.now()
         self.assertLess(step.course.created_at, now)
+
+class CourseViewsTest(TestCase):
+    def setUp(self):
+        self.course = Course.objects.create(
+            title="Python Regular Expressions",
+            description="Learn to write regular expressions in Python"
+        )
+        self.course2 = Course.objects.create(
+            title="Another course",
+            description="Another Python course"
+        )
+        self.step = Step.objects.create(
+            title="Step 1",
+            description="This is step 1",
+            course=self.course
+        )
+
+    def test_course_list_view(self):
+        resp = self.client.get(reverse('courses:list'))
+        self.assertEqual(resp.status_code, 200)
+        self.assertIn(self.course, resp.context['courses'])
+        self.assertIn(self.course2, resp.context['courses'])
