ad04c3e1770b8da4ce2cbc99f5d23cd6eee7c1eb
David Lin <David.Lin@bmwna.com>
2017-01-02 13:52:00 -0600

    Django learning site: add unit test for Course and Step models

--- python/django/learning_site/courses/tests.py
+++ python/django/learning_site/courses/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
+from django.utils import timezone
 
 # Create your tests here.
+from .models import Course
+
+class CourseModelTests(TestCase):
+    def test_course_creation(self):
+        course = Course.objects.create(
+            title="Python Regular Expressions",
+            description="Learn to write regular expressions in Python"
+        )
+        now = timezone.now()
+        self.assertLess(course.created_at, now)
+
+class StepModelTests(TestCase):
+    def test_course_creation(self):
+        course = Course.objects.create(
+            title="Step 1",
+            description="This is step 1"
+        )
+        now = timezone.now()
+        self.assertLess(course.created_at, now)
