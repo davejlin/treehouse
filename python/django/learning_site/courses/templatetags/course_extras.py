b975d427678186eadcace454f84f4eb08b555c03
David Lin <David.Lin@bmwna.com>
2017-01-04 22:18:26 -0600

    Django learning site: custom inclusion tag

--- python/django/learning_site/courses/templatetags/course_extras.py
+++ python/django/learning_site/courses/templatetags/course_extras.py
@@ -6,5 +6,11 @@ register = template.Library()
 
 @register.simple_tag
 def newest_course():
-    ''' Gets the most recent courses that was added to the library '''
+    ''' Gets the most recent course that was added to the library '''
     return Course.objects.latest('created_at')
+
+@register.inclusion_tag('courses/course_nav.html')
+def nav_courses_list():
+    ''' Returns dictionary of courses to display as navigation pane '''
+    courses = Course.objects.all()
+    return {'courses': courses}
