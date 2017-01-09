f02ad491fe2605f24f328679f133afab71e704d5
David Lin <David.Lin@bmwna.com>
2017-01-08 20:26:09 -0600

    Django learning site forms: add honeypot and clean_honeypot

--- python/django/learning_site_forms/learning_site/forms.py
+++ python/django/learning_site_forms/learning_site/forms.py
@@ -4,3 +4,13 @@ class SuggestionForm(forms.Form):
     name = forms.CharField()
     email = forms.EmailField()
     suggestions = forms.CharField(widget=forms.Textarea)
+    honeypot = forms.CharField(required=False,
+                               widget=forms.HiddenInput,
+                               label="Leave empty")
+
+    def clean_honeypot(self):
+        honeypot = self.cleaned_data['honeypot']
+        if len(honeypot):
+            raise forms.ValidationError(
+                "honeypot should be left empty. Bad bot!")
+        return honeypot
