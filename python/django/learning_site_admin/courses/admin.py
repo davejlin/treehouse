a4546de6b7cdee8fc2928a90c6d57f33baf127cd
David Lin <David.Lin@bmwna.com>
2017-01-24 15:43:28 -0600

    Django admin: add admin inlines, specific order of fields in QuizAdmin

--- python/django/learning_site_admin/courses/admin.py
+++ python/django/learning_site_admin/courses/admin.py
@@ -3,11 +3,35 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Course)
+class TextInline(admin.StackedInline):
+    model = models.Text
+
+
+class QuizInline(admin.StackedInline):
+    model = models.Quiz
+
+
+class AnswerInline(admin.StackedInline):
+    model = models.Answer
+
+
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [TextInline, QuizInline]
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [AnswerInline]
+
+
+class QuizAdmin(admin.ModelAdmin):
+    fields = ['course', 'title', 'description', 'order', 'total_questions']
+
+
+admin.site.register(models.Course, CourseAdmin)
 admin.site.register(models.Text)
-admin.site.register(models.Quiz)
-admin.site.register(models.MultipleChoiceQuestion)
-admin.site.register(models.TrueFalseQuestion)
+admin.site.register(models.Quiz, QuizAdmin)
+admin.site.register(models.MultipleChoiceQuestion, QuestionAdmin)
+admin.site.register(models.TrueFalseQuestion, QuestionAdmin)
 admin.site.register(models.Answer)
 
 admin.site.site_header = "Learning Site administration"
