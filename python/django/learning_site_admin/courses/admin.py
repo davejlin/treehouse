60e609e40467862e9eeb5330fbc5a2237675e67f
David Lin <David.Lin@bmwna.com>
2017-01-25 21:44:57 -0600

    Django admin: add custom simple list filter

--- python/django/learning_site_admin/courses/admin.py
+++ python/django/learning_site_admin/courses/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from datetime import date
 
 from . import models
 
@@ -15,10 +16,34 @@ class AnswerInline(admin.StackedInline):
     model = models.Answer
 
 
+class YearListFilter(admin.SimpleListFilter):
+    title = 'year created'
+    parameter_name = 'year'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('2015', '2015'),
+            ('2016', '2016')
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == '2015':
+            return queryset.filter(
+                created_at__gte=date(2015, 1, 1),
+                created_at__lte=date(2015, 12, 31)
+            )
+
+        if self.value() == '2016':
+            return queryset.filter(
+                created_at__gte=date(2016, 1, 1),
+                created_at__lte=date(2016, 12, 31)
+            )
+
+
 class CourseAdmin(admin.ModelAdmin):
     inlines = [TextInline, QuizInline]
     search_fields = ['title', 'description']
-    list_filter = ['created_at', 'published']
+    list_filter = ['created_at', 'published', YearListFilter]
 
 class QuestionAdmin(admin.ModelAdmin):
     inlines = [AnswerInline]
