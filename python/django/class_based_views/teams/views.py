501cf3bac5010ebf1e4fbabed9a89ddde85bc272
David Lin <David.Lin@bmwna.com>
2017-01-29 16:42:02 -0600

    Django class-based views: add CRUD operation views

--- python/django/class_based_views/teams/views.py
+++ python/django/class_based_views/teams/views.py
@@ -1,5 +1,9 @@
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.views.generic import (
+    ListView, DetailView,
+    CreateView, UpdateView, DeleteView
+)
 
 from . import models
 
@@ -21,3 +25,18 @@ class TeamListView(ListView):
 
 class TeamDetailView(DetailView):
     model = models.Team
+
+
+class TeamCreateView(CreateView):
+    model = models.Team
+    fields = ('name', 'practice_location', 'coach')
+
+
+class TeamUpdateView(UpdateView):
+    model = models.Team
+    fields = ('name', 'practice_location', 'coach')
+
+
+class TeamDeleteView(DeleteView):
+    model = models.Team
+    success_url = reverse_lazy("teams:list")
