5267705ac028d160fd796e9d9735dc1671681070
David Lin <dave.j.lin@gmail.com>
2017-02-08 16:15:48 -0600

    Python Django REST: add POST

--- python/django/ed_reviews_REST/courses/views.py
+++ python/django/ed_reviews_REST/courses/views.py
@@ -1,3 +1,4 @@
+from rest_framework import status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
@@ -6,8 +7,14 @@ from . import serializers
 
 # Create your views here.
 
-class ListCourse(APIView):
+class ListCreateCourse(APIView):
     def get(self, request, format=None):
         courses = models.Course.objects.all()
         serializer = serializers.CourseSerializer(courses, many=True)
         return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = serializers.CourseSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
