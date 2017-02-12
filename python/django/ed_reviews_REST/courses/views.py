d7171103f5d2a6665997b27363d259bd3c9a3703
David Lin <dave.j.lin@gmail.com>
2017-02-11 20:31:49 -0600

    Django REST: use mixing to remove List for reviews

--- python/django/ed_reviews_REST/courses/views.py
+++ python/django/ed_reviews_REST/courses/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
+from rest_framework import mixins
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
@@ -55,6 +56,10 @@ class CourseViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
 
-class ReviewViewSet(viewsets.ModelViewSet):
+class ReviewViewSet(mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.DestroyModelMixin,
+                    viewsets.GenericViewSet):
     queryset = models.Review.objects.all()
     serializer_class = serializers.ReviewSerializer
