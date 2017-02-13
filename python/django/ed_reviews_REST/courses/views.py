09b82ac7a349409e04549c3857e620c818b65bd0
David Lin <dave.j.lin@gmail.com>
2017-02-13 15:49:49 -0600

    Django REST: add permissions to CourseViewSet

--- python/django/ed_reviews_REST/courses/views.py
+++ python/django/ed_reviews_REST/courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework import permissions
 from rest_framework import viewsets
 from rest_framework.decorators import detail_route
 from rest_framework.response import Response
@@ -45,6 +46,7 @@ class RetrieveUpdateDestroyReview(generics.RetrieveUpdateDestroyAPIView):
 
 
 class CourseViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.DjangoModelPermissions,)
     queryset = models.Course.objects.all()
     serializer_class = serializers.CourseSerializer
 
