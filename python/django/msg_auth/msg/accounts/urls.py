cdfd1be62dce9e5259654970fbe149af5bdc6f57
David Lin <dave.j.lin@gmail.com>
2017-03-02 09:19:20 -0600

    Django auth:  signup using Django’s UserCreationForm

--- python/django/msg_auth/msg/accounts/urls.py
+++ python/django/msg_auth/msg/accounts/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
 	# url(r"login/$", views.LoginView.as_view(), name="login"),
     url(r"logout/$", views.LogoutView.as_view(), name="logout"),
+    url(r"signup/$", views.SignUp.as_view(), name="signup"),
 ]
