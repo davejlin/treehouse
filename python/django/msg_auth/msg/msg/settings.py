c7beb98e93b87cbaa679a2548fe65b4865365a26
David Lin <dave.j.lin@gmail.com>
2017-03-02 09:56:28 -0600

    Django auth: password reset

--- python/django/msg_auth/msg/msg/settings.py
+++ python/django/msg_auth/msg/msg/settings.py
@@ -129,3 +129,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets')]
 
 LOGIN_REDIRECT_URL = "posts:all"
+
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
