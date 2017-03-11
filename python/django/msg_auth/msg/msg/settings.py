0336d6a15352b27be3d0f17069c47f729b621473
David Lin <dave.j.lin@gmail.com>
2017-03-10 19:11:25 -0600

    Django auth: custom user model

--- python/django/msg_auth/msg/msg/settings.py
+++ python/django/msg_auth/msg/msg/settings.py
@@ -132,3 +132,5 @@ LOGIN_REDIRECT_URL = "posts:all"
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+
+AUTH_USER_MODEL = "accounts.User"
