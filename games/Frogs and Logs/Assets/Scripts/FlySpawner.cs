16ed382ae3563cd94c61f16a69a15ca9911f0e05
David Lin <dave.j.lin@gmail.com>
2017-04-10 12:20:02 -0500

    Unity Frogs and Logs: add fly spawner to game manager

---
+++ games/Frogs and Logs/Assets/Scripts/FlySpawner.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlySpawner : MonoBehaviour {
+
+	[SerializeField]
+	private GameObject flyPrefab;
+
+	[SerializeField]
+	private int totalFlyMinimum = 12;
+
+	private float spawnArea = 25f;
+
+	public static int totalFlies;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
