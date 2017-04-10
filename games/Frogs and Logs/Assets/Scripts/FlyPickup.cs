35b92f128d75ae7e05ed5a5ca1b7d1a0b9570c99
David Lin <dave.j.lin@gmail.com>
2017-04-10 10:59:24 -0500

    Unity Frogs and Logs: script fly pickup

---
+++ games/Frogs and Logs/Assets/Scripts/FlyPickup.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlyPickup : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		if (other.CompareTag ("Player")) {
+			Destroy (gameObject);
+		}
+	}
+}
