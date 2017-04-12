47301d8c31ff1c9cbb48a78a60ab476f852bb801
David Lin <dave.j.lin@gmail.com>
2017-04-12 15:57:15 -0500

    Unity Frogs and Logs: random sound player script

---
+++ games/Frogs and Logs/Assets/Scripts/RandomSoundPlayer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSoundPlayer : MonoBehaviour {
+
+	[SerializeField]
+	private List<AudioClip> soundClips = new List<AudioClip>();
+
+	private AudioSource audioSource;
+	private float soundTimerDelay = 3f;
+	private float soundTimer;
+
+	// Use this for initialization
+	void Start () {
+		audioSource = GetComponent<AudioSource> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		soundTimer += Time.deltaTime;
+		if (soundTimer >= soundTimerDelay) {
+			soundTimer = 0f;
+			AudioClip randomSound = soundClips[Random.Range (0, soundClips.Count)];
+			audioSource.PlayOneShot (randomSound);
+		}
+	}
+}
