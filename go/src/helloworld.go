f2f462cae4101e7da2f4eda98c9fa5dfbc7e0269
David Lin <dave.j.lin@gmail.com>
2017-07-13 16:05:10 -0500

    go: variables

--- go/src/helloworld.go
+++ go/src/helloworld.go
@@ -3,11 +3,20 @@ package main
 import (
 	"fmt"
 	"github.com/golang/example/stringutil"
+	"reflect"
+	"net"
+	"time"
 )
 
 func main() {
 	fmt.Println("Hello world!")
 	fmt.Println(otherFuction())
+	fmt.Println(reflect.TypeOf(1))
+	fmt.Println(reflect.TypeOf(1.0))
+	fmt.Println(reflect.TypeOf("1.0"))
+	fmt.Println(reflect.TypeOf(true))
+	fmt.Println(reflect.TypeOf(net.IPv4(127, 0, 0, 1)))
+	fmt.Println(reflect.TypeOf(time.Now()))
 }
 
 func otherFuction() string {
--- go/src/temp.go
+++ go/src/temp.go
@@ -13,4 +13,16 @@ func main() {
 	roundedDown := math.Floor(myNumber)
 	fmt.Println(roundedUp, roundedDown)
 	fmt.Println(welcome.English)
+
+	var a int
+	a = 1
+	var b, c int
+	b, c = 2, 3
+	var d = 5
+	e, f := 6, 7
+	fmt.Println(a, b, c, d, e, f)
+
+	var anInt int = 1
+	var aFloat float64 = float64(anInt)
+	fmt.Println(anInt, aFloat)
 }
