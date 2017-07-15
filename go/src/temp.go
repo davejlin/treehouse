c548bff9eedf2293169afc7777c55ad428aa1da2
David Lin <dave.j.lin@gmail.com>
2017-07-15 14:30:37 -0500

    go: if

--- go/src/temp.go
+++ go/src/temp.go
@@ -53,6 +53,39 @@ func main() {
 		fmt.Println(i)
 	}
 
+	if true {
+		fmt.Println("true")
+	}
+
+	if false {
+		fmt.Println("false")
+	}
+
+	if 1 < 2 {
+		fmt.Println("1 < 2")
+	}
+
+	if 1 > 2 {
+		fmt.Println("1 > 2")
+	}
+
+	if true && true {
+		fmt.Println("true && true")
+	}
+
+	if true || false {
+		fmt.Println("true || false")
+	}
+
+	if true {
+		fmt.Println("true")
+	} else if false {
+		fmt.Println("false")
+	} else {
+		fmt.Println("else")
+	}
+
+
 }
 
 func myFunction() {
