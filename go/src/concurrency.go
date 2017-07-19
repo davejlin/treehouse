52ddd1e6521f9013f457334adb4ec7fab19e6435
David Lin <dave.j.lin@gmail.com>
2017-07-19 15:43:55 -0500

    go: concurrency channels

--- go/src/concurrency.go
+++ go/src/concurrency.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"time"
+	"math/rand"
 )
 
 func longTask() {
@@ -11,9 +12,27 @@ func longTask() {
 	fmt.Println("Long task finished")
 }
 
+func longTaskRandom(channel chan int) {
+	delay := rand.Intn(5)
+	fmt.Println("Staring long task")
+	time.Sleep(time.Duration(delay) * time.Second)
+	fmt.Println("Long task finished")
+	channel <- delay
+}
+
 func main() {
-	go longTask()
-	go longTask()
-	go longTask()
-	time.Sleep(4 * time.Second)
+	// go longTask()
+	// go longTask()
+	// go longTask()
+	// time.Sleep(4 * time.Second)
+
+	rand.Seed(time.Now().Unix())
+	channel := make(chan int)
+	for i := 1; i <= 3; i++ {
+		go longTaskRandom(channel)
+	}
+
+	for i := 1; i <= 3; i++ {
+		fmt.Println("Took", <-channel, "seconds")
+	}
 }
