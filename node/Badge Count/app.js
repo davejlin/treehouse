11b3941ce1857528a7e9f674b6b77f567ba7eafb
David Lin <dave.j.lin@gmail.com>
2018-06-12 15:39:21 -0500

    node wrap functionality into getProfile function

--- node/Badge Count/app.js
+++ node/Badge Count/app.js
@@ -1,7 +1,6 @@
 // Problem:  Look at a user's badge count and JavaScript points
 // Solution:  Use Node.js to connect to Treehouse's API to get profile information to print out
 const https = require('https');
-const username = "davejlin";
 
 function printMessage(username, badgeCount, points) {
 	const message = `${username} has ${badgeCount} total badge(s) and ${points} points in JavaScript`;
@@ -10,20 +9,26 @@ function printMessage(username, badgeCount, points) {
 
 printMessage("testUser", 100, 2000000);
 
+function getProfile(username) {
 // Connect to the API URL (https://teamtreahouse.com/username.json)
-const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
-	console.log(`response status code = ${response.statusCode}`);
-	
-	let body = "";
-	// Read the data
-	response.on('data', data => {
-		body += data.toString();
-	});
+	const request = https.get(`https://teamtreehouse.com/${username}.json`, response => {
+		console.log(`response status code = ${response.statusCode}`);
+		
+		let body = "";
+		// Read the data
+		response.on('data', data => {
+			body += data.toString();
+		});
 
-	response.on('end', () => {
-		// Parse the data
-		const profile = JSON.parse(body);
-		// Print the data
-		printMessage(username, profile.badges.length, profile.points.JavaScript);
+		response.on('end', () => {
+			// Parse the data
+			const profile = JSON.parse(body);
+			// Print the data
+			printMessage(username, profile.badges.length, profile.points.JavaScript);
+		});
 	});
-});
+}
+
+const users = ["davejlin", "chalkers", "alenaholligan", "davemcfarland"];
+
+users.forEach(getProfile);
