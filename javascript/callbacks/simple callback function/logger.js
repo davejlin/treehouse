e4ae943748fc1f75ebac0a92dab16a21323457f8
David Lin <dave.j.lin@gmail.com>
2018-06-19 08:46:04 -0500

    javascript callbacks base example

---
+++ javascript/callbacks/simple callback function/logger.js
@@ -0,0 +1,10 @@
+const person = {
+    name: 'David Lin',
+    role: 'JavaScript Teacher'
+}
+
+function logTeacher(teacher) {
+    console.log(`${teacher.name} - ${teacher.role}`);
+}
+
+logTeacher(person);
